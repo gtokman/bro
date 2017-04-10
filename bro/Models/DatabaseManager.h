6611d9f6dcfb22fffa920628e3ad13815ff6b080
gtokman <gtokman@fullsail.edu>
2017-04-09 20:19:12 -0400

    added database refs

---
+++ bro/Models/DatabaseManager.h
@@ -0,0 +1,13 @@
+//
+//  DatabaseManager.h
+//  bro
+//
+//  Created by g tokman on 4/9/17.
+//  Copyright © 2017 garytokman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DatabaseManager : NSObject
+
+@end
---
+++ bro/Models/DatabaseManager.m
@@ -0,0 +1,13 @@
+//
+//  DatabaseManager.m
+//  bro
+//
+//  Created by g tokman on 4/9/17.
+//  Copyright © 2017 garytokman. All rights reserved.
+//
+
+#import "DatabaseManager.h"
+
+@implementation DatabaseManager
+
+@end
