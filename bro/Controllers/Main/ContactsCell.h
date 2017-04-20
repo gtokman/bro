1730f5e5b2e0c46a61720e572da63d54de80d97d
gtokman <gtokman@fullsail.edu>
2017-04-19 21:59:17 -0400

    Added contacts VC

---
+++ bro/Controllers/Main/ContactsCell.h
@@ -0,0 +1,13 @@
+//
+//  ContactsCell.h
+//  bro
+//
+//  Created by g tokman on 4/19/17.
+//  Copyright © 2017 garytokman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactsCell : UITableViewCell
+
+@end
