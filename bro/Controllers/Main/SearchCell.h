34cd7a89f76b1ea1feed242e2486ce3eece1e94d
gtokman <gtokman@fullsail.edu>
2017-04-21 18:11:17 -0400

    Added custom search cell

---
+++ bro/Controllers/Main/SearchCell.h
@@ -0,0 +1,13 @@
+//
+//  SearchCell.h
+//  bro
+//
+//  Created by g tokman on 4/21/17.
+//  Copyright © 2017 garytokman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchCell : UITableViewCell
+
+@end
---
+++ bro/Controllers/Main/SearchCell.m
@@ -0,0 +1,24 @@
+//
+//  SearchCell.m
+//  bro
+//
+//  Created by g tokman on 4/21/17.
+//  Copyright © 2017 garytokman. All rights reserved.
+//
+
+#import "SearchCell.h"
+
+@implementation SearchCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
