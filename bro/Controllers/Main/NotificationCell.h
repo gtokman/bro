8f36591eeddd28b096e7991c3d31c0c9d2b5abc9
gtokman <gtokman@fullsail.edu>
2017-04-22 15:46:56 -0400

    Added notification cell

---
+++ bro/Controllers/Main/NotificationCell.h
@@ -0,0 +1,13 @@
+//
+//  NotificationCell.h
+//  bro
+//
+//  Created by g tokman on 4/22/17.
+//  Copyright © 2017 garytokman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationCell : UITableViewCell
+
+@end
---
+++ bro/Controllers/Main/NotificationCell.m
@@ -0,0 +1,24 @@
+//
+//  NotificationCell.m
+//  bro
+//
+//  Created by g tokman on 4/22/17.
+//  Copyright © 2017 garytokman. All rights reserved.
+//
+
+#import "NotificationCell.h"
+
+@implementation NotificationCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
